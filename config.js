3c3ff4bc2d82e761a47ff89fd0560a2dad771946
ɪ ᴍ ᴇ ʟ <100808225+imellChan@users.noreply.github.com>
2022-03-25 12:32:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,8 +48,8 @@ global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© I M E L C H A N' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
+global.image = 'https://telegra.ph/file/0ef70cbf2f013c4b9faa0.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/0ef70cbf2f013c4b9faa0.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
